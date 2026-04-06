104ac0e6913d460434f1dd6cf1c34eb4b94a70b8
Taehong <lth1283910@gmail.com>
2026-04-06 15:54:37 +0900

    Update Google Analytics tag to new account (G-P04TH8XJJ9)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,12 +51,12 @@ export default function RootLayout({
         <meta name="theme-color" content="#7c5cbf" />
         <meta name="author" content="Loan Calculator Tool" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P08T3SZDQH"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P04TH8XJJ9"></script>
         <script dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-P08T3SZDQH');`
+gtag('config', 'G-P04TH8XJJ9');`
         }} />
       </head>
       <body className="bg-white text-gray-900 antialiased">
